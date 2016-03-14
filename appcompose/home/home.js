094d16e03505caa1a8a5194856f0c110072b1881
Jessica Wong <jessawong@outlook.com>
2016-03-13 20:22:27 -0700

    removed redundancies

--- appcompose/home/home.js
+++ appcompose/home/home.js
@@ -95,26 +95,4 @@
       }
     }
 
-
-  
-
- /* Displays the "Subject" and "From" fields, based on the current mail item
-  function displayItemDetails(){
-    var item = Office.cast.item.toItemRead(Office.context.mailbox.item);
-    //jQuery('#subject').text(item.subject);
-
-    var from;
-    if (item.itemType === Office.MailboxEnums.ItemType.Message) {
-      from = Office.cast.item.toMessageRead(item).from;
-    } else if (item.itemType === Office.MailboxEnums.ItemType.Appointment) {
-      from = Office.cast.item.toAppointmentRead(item).organizer;
-    }
-
-    if (from) {
-      jQuery('#pbeName').text(from.displayName);
-      /*jQuery('#from').click(function(){
-        app.showNotification(from.displayName, from.emailAddress);
-      });
-    }
-  }*/
 })();
