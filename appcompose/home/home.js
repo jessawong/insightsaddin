f7c0da2894d0e492434bfb97e4aa6f7ae54672ca
Jessica Wong <jessawong@outlook.com>
2016-03-14 22:41:16 -0700

    added azure functionality

--- appcompose/home/home.js
+++ appcompose/home/home.js
@@ -7,8 +7,23 @@
                     {'type':'Intelligent Cloud', 'url':'../../images/cloud.png', 'alias':'US_DX_RISV_CLOUD@microsoft.com', 'fadeUrl': '../../images/cloudOverlay.png', 'intel': true}];
   var engageType = ['Briefing', 'Envisioning', 'ADS', 'Hackfest/PoC', 'Delivery', 'Other'];
   var info = {'pbe':'', 'website': '', 'date':'', 'time':'', 'reason':'', 'meeting':'Skype', 'location':'', 'engagement':'', 'crm': '', 'stage': ''};
+  var cloudInfo = {'status': '', 'provider':'', 'consumption':'', 'workloads':''};
   var crmStage = ['0%', '10%', '20%', '40%', '60%', '80%', '95%', '100%'];
   var time = ['30 min', '60 min', '90 min', '120 min', '2+ hours'];
+  var status = ['New', 'Experimenting', 'Hybrid', 'Running'];
+  var provider = ['Azure', 'AWS', 'Google', 'Other'];
+  var consumptionLevel = ['<25k', '25k-99k', '100k-499k', '500k+'];
+  var workLoads = [{'Compute': false}, 
+                    {'Web & Mobile': false},
+                    {'Data & Storage': false},
+                    {'Analytics': false},
+                    {'Internet of Things': false},
+                    {'Networking': false},
+                    {'Media & CDN': false},
+                    {'Hybrid Integration': false},
+                    {'Identity & Access Management': false},
+                    {'Dev Services': false},
+                    {'Management & Security': false}];
 
 
     // The Office initialize function must be run each time a new page is loaded
@@ -17,46 +32,75 @@
       info.pbe = Office.context.mailbox.userProfile.emailAddress;
       Office.context.mailbox.item.subject.setAsync("TE Request");
       $(".valign").hover(function() {
-        $(this).stop().animate({"opacity":"0.5"}, "slow");
+        $(this).stop().animate({"opacity":"0.5"}, "fast");
       },
       function() {
-        $(this).stop().animate({"opacity":"1"}, "slow");
+        $(this).stop().animate({"opacity":"1"}, "fast");
       });
     });
   };
 
-    app.controller('FormController', function() {
+    app.controller('FormController', function($scope) {
         this.technology = selectTech;
         this.engagement = engageType;
+        this.cloudInfor = cloudInfo;
         this.showTech = true;
+        this.cloudStatus = status;
+        this.cloudProv = provider;
+        this.consumption = consumptionLevel;
+        this.workloads = workLoads;
         this.information = info;
         this.timeOptions = time;
         this.skype = false;
+        this.intelCloud = false;
+        this.showCont = false;
         this.ads = false;
         this.stage = crmStage;
         this.goBack = function() {
           this.skype = false;
           this.showTech = true;
+          this.intelCloud = false;
+          this.showCont = false;
           hideStatus();
           this.information["Technology"] = "";
+          reset();
         };
         this.setTech = function(option) {
           console.log(this.information);
           this.information["Technology"] = option.type;
           Office.context.mailbox.item.to.setAsync([option.alias]);
           this.showTech = false;
+          if (option.type === "Intelligent Cloud") {
+            this.intelCloud = true;
+          }
+        };
+
+        this.cont = function() {
+          this.showCont = true;
         };
 
         this.addRequest = function() {
+          var cloudDetails = '';
+          if (this.intelCloud) {
+            cloudDetails = "<br/><h4>Industry/Verticle: </h4>" + this.cloudInfor.industry + "<br/><h4>Cloud Status: </h4>" + this.cloudInfor.status
+                            + "<br/><h4>Cloud Provider: </h4>" + this.cloudInfor.provider + "<br/><h4>Annual Consumption: </h4>" + this.cloudInfor.consumption
+                            + "<br/><h4>Workdloads: </h4>";
+            for (var key in this.workloads) {
+              if (this.workLoads[key]) {
+                cloudDetails += key + "<br/>";
+              }
+            }
+          }
           Office.context.mailbox.item.body.setAsync("<h4>Product's Website: </h4>" + this.information.website + "<br/><h4>CRM Link: </h4>" + this.information.crm
                                                       + "<br/><h4>Stage:</h4>" + this.information.stage + "<br/><h4>Engagement Requested: </h4>" 
                                                       + this.information.engagement + "<br/><h4>Requested Date for Engagement:</h4>" + this.information.date 
                                                       + "<br/><h4>Reason:</h4>" + this.information.reason + "<br/><h4>Duration of meeting:</h4>" 
                                                       + this.information.time + "<br/><h4>Location:</h4>" + this.information.location + "<br/><h4>Meeting:</h4>" 
-                                                      + this.information.meeting, {coercionType: "html"});
+                                                      + this.information.meeting + cloudDetails, {coercionType: "html"});
           reset();
           showStatus();
           this.information = info;
+          this.workloads = workLoads;
           this.skype = false;
         };
         this.canSkype = function() {
@@ -65,6 +109,8 @@
 
         this.goHome = function() {
           this.showTech = true;
+          this.intelCloud = false;
+          this.showCont = false;
           showForm();
           hideStatus();
         };
@@ -92,6 +138,9 @@
             info[key] = "";
           }
         }
+      }
+      for (var key in workLoads) {
+        workLoads[key] = false;
       }
     }
 
