89cdfed1de653d4774b4aff14f1525770c3c799a
Jessica Wong <jessawong@outlook.com>
2016-08-30 12:48:46 -0700

    fixing back button

--- appcompose/home/home.js
+++ appcompose/home/home.js
@@ -3,20 +3,10 @@
 
   var app = angular.module('readHome', []);
   //var selectTech = [{'type':'Intelligent Cloud', 'url':'../../images/cloud.png', 'alias':'usdxrisvintelligentcloudteam@service.microsoft.com', 'fadeUrl': '../../images/cloudOverlay.png', 'intel': true}];
-  //var engageType = ['Briefing', 'Envisioning', 'ADS', 'Hackfest/PoC', 'Other'];
   var info = {'pbe': '', 'website': '', 'date':'', 'time':'', 'reason':'', 'meeting':'Skype', 'location':'', 'engagement':'', 'crm': '', 'stage': ''};
   var cloudInfo = {'status': '', 'provider':'', 'consumption':'', 'workloads':'', 'vteam':''};
-  //var crmStage = ['0%', '10%', '20%', '40%', '60%', '80%', '95%', '100%'];
-  //var time = ['30 min', '60 min', '90 min', '120 min', '2+ hours'];
- // var status = ['New', 'Experimenting', 'Hybrid', 'Running'];
-  //var provider = ['None','Azure', 'AWS', 'Google', 'Other'];
- // var consumptionLevel = ['<25k', '25k-99k', '100k-499k', '500k+'];
- // var workLoads = ['Modern Datacenter(IT Pro)', 'Data Platform & Analytics', 'Modern Apps (Cloud Dev)'];
-  //var secWorkLoads = {'Identity & Access Mgt': false, 'Compute':false, 'Networking':false, 'Storage & DR':false, 'Big Data & SQL':false,'IoT':false,'Advanced Analytics':false, 'PaaS Services':false,'OSS Platforms':false, 'Mobility':false, 'Media Solutions':false};
-    // The Office initialize function must be run each time a new page is loaded
   Office.initialize = function(reason){
     jQuery(document).ready(function(){
-      
       
       info.pbe = Office.context.mailbox.userProfile.displayName;
       //$("#pbeName").text(info.pbe);
@@ -37,7 +27,6 @@
         $scope.technology = [{'type':'Intelligent Cloud', 'url':'../../images/cloud.png', 'alias':'usdxrisvintelligentcloudteam@service.microsoft.com', 'fadeUrl': '../../images/cloudOverlay.png', 'intel': true}];
         $scope.engagement = ['Briefing', 'Envisioning', 'ADS', 'Hackfest/PoC', 'Other'];;
         $scope.cloudInfor = {'status': '', 'provider':'', 'consumption':'', 'workloads':'', 'vteam':''};
-        this.showTech = true;
         $scope.cloudStatus = ['New', 'Experimenting', 'Hybrid', 'Running'];
         $scope.cloudProv = ['None','Azure', 'AWS', 'Google', 'Other'];
         $scope.consumption = ['<25k', '25k-99k', '100k-499k', '500k+'];
@@ -55,40 +44,12 @@
         $scope.stages = ['0%', '10%', '20%', '40%', '60%', '80%', '95%', '100%'];
         $scope.showMain = true;
         $scope.goBack = function() {
-          //if ($scope.showCont) {
-            $scope.showCont = false;
-            $scope.showMain = true;
-            $scope.showArrow = true;
-         /* } else {
-            $scope.skype = false;
-            this.showTech = true;
-            $scope.intelCloud = false;
-            $scope.showCont = false;
-            $scope.showMain = false;
-            $scope.showArray = false;
-            $scope.showBack = false;
-            $scope.showArrow = false;
-            hideStatus();
-            $scope.information["Technology"] = "";
-            reset();
-          }*/
+          $scope.showCont = false;
+          $scope.showMain = true;
+          $scope.showArrow = true;
+          $scope.showBack = false;
           $scope.showSubmit = false;
         };
-       /* this.setTech = function(option) {
-          console.log(this.technology[option]);
-          //this.information["Technology"] = this.technology[option].type;
-         if (this.technology[option].type === "Intelligent Cloud") {
-            this.intelCloud = true;
-            this.showArrow = true;
-            this.showSubmit = false;
-          } else {
-            this.showSubmit = true;
-          }
-          Office.context.mailbox.item.to.setAsync([this.technology[option].alias]);
-          //this.showTech = false;
-          this.showMain = true;
-          this.showBack = true;
-        };*/
 
         $scope.setWork = function(option) {
           option.value = !option.value;
@@ -166,8 +127,6 @@
                    console.log(JSON.stringify(payload));
            }); 
 
-          //reset();
-          //$scope.information = info;
           $scope.showBack = false;
           $scope.workloads = workLoads;
           $scope.skype = false;
@@ -176,15 +135,6 @@
           $scope.skype = !$scope.skype;
         };
 
-        /*this.goHome = function() {
-          this.showTech = true;
-          this.intelCloud = false;
-          this.showCont = false;
-          this.showMain = false;
-          this.showArrow = false;
-          showForm();
-          hideStatus();
-        };*/
     });
 
     function showStatus(domId) {
@@ -198,21 +148,6 @@
 
     function showForm() {
       $("#form").show();
-    }
-
-   function reset() {
-      for (var key in info) {
-        if (key != "pbe") {
-          if (key === "meeting") {
-            info[key] = "Skype";
-          } else {
-            info[key] = "";
-          }
-        }
-      }
-      for (var key in secWorkLoads) {
-        secWorkLoads[key] = false;
-      }
     }
 
 })();
