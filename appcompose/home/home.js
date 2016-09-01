6e319e8ab1785abbc5ed0eb5ec5c2e070bf06375
Greg Degruy <gregory.degruy@gmail.com>
2016-09-01 13:23:50 -0700

    Attemoting schema update
    BUG: informattion and cloudInfor not defined

--- appcompose/home/home.js
+++ appcompose/home/home.js
@@ -2,23 +2,13 @@
   'use strict';
 
   var app = angular.module('readHome', []);
-  //var selectTech = [{'type':'Intelligent Cloud', 'url':'../../images/cloud.png', 'alias':'usdxrisvintelligentcloudteam@service.microsoft.com', 'fadeUrl': '../../images/cloudOverlay.png', 'intel': true}];
-  var info = {'pbe': '', 'website': '', 'date':'', 'time':'', 'reason':'', 'meeting':'Skype', 'location':'', 'engagement':'', 'crm': '', 'stage': ''};
+  var info = {'pbe': '', 'si': '', 'isv':'', 'website': '', 'date':'', 'time':'', 'reason':'', 'meeting':'Skype', 'city':'', 'state':'', 'engagement':'', 'crm': '', 'stage': ''};
   var cloudInfo = {'status': '', 'provider':'', 'consumption':'', 'workloads':'', 'vteam':''};
   Office.initialize = function(reason){
     jQuery(document).ready(function(){
       
       info.pbe = Office.context.mailbox.userProfile.displayName;
-      //$("#pbeName").text(info.pbe);
-      //Office.context.mailbox.item.subject.setAsync("Intelligent Cloud TE Request");
-      
-     // Office.context.mailbox.item.to.setAsync([selectTech[0].alias]);
-     /* $(".valign").hover(function() {
-        $(this).stop().animate({"opacity":"1.0"}, "fast");
-      },
-      function() {
-        $(this).stop().animate({"opacity":"0.0"}, "fast");
-      });*/
+     
     });
   };
 
@@ -32,6 +22,7 @@
         $scope.consumption = ['<25k', '25k-99k', '100k-499k', '500k+'];
         $scope.primeWorkloads = ['Modern Datacenter(IT Pro)', 'Data Platform & Analytics', 'Modern Apps (Cloud Dev)'];
         $scope.secondWorkloads = {'Identity & Access Mgt': false, 'Compute':false, 'Networking':false, 'Storage & DR':false, 'Big Data & SQL':false,'IoT':false,'Advanced Analytics':false, 'PaaS Services':false,'OSS Platforms':false, 'Mobility':false, 'Media Solutions':false};
+        $scope.workloads = {'Identity & Access Mgt': false, 'Compute':false, 'Networking':false, 'Storage & DR':false, 'Big Data & SQL':false,'IoT':false,'Advanced Analytics':false, 'PaaS Services':false,'OSS Platforms':false, 'Mobility':false, 'Media Solutions':false};
         $scope.information = info;
         $scope.timeOptions = ['30 min', '60 min', '90 min', '120 min', '2+ hours'];
         $scope.skype = false;
@@ -70,37 +61,39 @@
             cloudDetails = "<br/><h4>Industry/Vertical: </h4>" + $scope.cloudInfor.industry + "<br/><h4>Cloud Status: </h4>" + this.cloudInfor.status
                             + "<br/><h4>Cloud Provider: </h4>" + $scope.cloudInfor.provider + "<br/><h4>Annual Consumption: </h4>" + this.cloudInfor.consumption;
             var workloadString = "[";
-            for (var key in $scope.secondWorkloads) {
-              if ($scope.secondWorkloads[key]) {
+            for (var key in $scope.workloads) {
+              if ($scope.workloads[key]) {
                 workloadString += key + ",";
               }
             }
             workloadString = workloadString.substr(0, workloadString.length - 1) + "]";
           }
           Office.context.mailbox.item.subject.setAsync("[" + $scope.cloudInfor.vteam + "] Intelligent Cloud TE Request");
-          Office.context.mailbox.item.body.setAsync("<h4>Product's Website: </h4>" + $scope.information.website + "<br/><h4>CRM Link: </h4>" + $scope.information.crm
+          // TODO: Update mail body with new shcema information
+          Office.context.mailbox.item.body.setAsync("<h4>SI: </h4>" + $scope.information.si + "<h4>ISV: </h4>" + $scope.information.isv + "<h4>Product's Website: </h4>" + $scope.information.website + "<br/><h4>CRM Link: </h4>" + $scope.information.crm
                                                       + "<br/><h4>Stage:</h4>" + $scope.information.stage + "<br/><h4>Engagement Requested: </h4>" 
                                                       + $scope.information.engagement + "<br/><h4>Requested Date for Engagement:</h4>" + $scope.information.date 
                                                       + "<br/><h4>Reason:</h4>" + $scope.information.reason + "<br/><h4>Duration of meeting:</h4>" 
                                                       + $scope.information.time + "<br/><h4>Location:</h4>" + $scope.information.location + "<br/><h4>Meeting:</h4>" 
-                                                      + $scope.information.meeting + cloudDetails + "<br/><h4>Secondary Workloads:</h4>" + workloadString, {coercionType: "html"});
-          //REDUNDANCY EVERYWHERE. NEED TO FIX.                                            
+                                                      + $scope.information.meeting + cloudDetails + "<br/><h4>Workloads:</h4>" + workloadString, {coercionType: "html"});
           var payload = { 
                                   "Pbe": info.pbe,
+                                  "SI": information.si,
+                                  "ISV": information.isv,
                                   "Website": information.website, 
                                   "Crm": information.crm, 
                                   "Stage": information.stage, 
                                   "EngagementType": information.engagement,
                                   "Date": information.date,
                                   "Reason": information.reason,
-                                  "Location": information.location,
+                                  "City": information.city,
+                                  "State": information.state,
                                   "Meeting": information.meeting,
                                   "Industry": cloudInfor.industry,
                                   "cloudStatus": cloudInfor.status,
                                   "cloudProvider": cloudInfor.provider,
                                   "consumption": cloudInfor.consumption,
-                                  "primaryWorkLoad": cloudInfor.vteam,
-                                  "secWorkLoads": workloadString
+                                  "workLoads": workloadString
                                 };
                                 
           var namespace = "insightsaddin-eh";
