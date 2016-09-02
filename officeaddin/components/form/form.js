5669969a54f23b20f18977975f68a832c35e61d6
Greg Degruy <gregory.degruy@gmail.com>
2016-09-02 08:06:56 -0700

    Removed testing text

--- officeaddin/components/form/form.js
+++ officeaddin/components/form/form.js
@@ -32,7 +32,6 @@
   };
 
   app.controller('FormController', function ($scope) {
-
     info["Technology"] = "Intelligent Cloud";
     $scope.technology = [{ 'type': 'Intelligent Cloud', 'url': '../../images/cloud.png', 'alias': 'usdxrisvintelligentcloudteam@service.microsoft.com', 'fadeUrl': '../../images/cloudOverlay.png', 'intel': true }];
     $scope.engagement = ['Briefing', 'Envisioning', 'ADS', 'Hackfest/PoC', 'Other'];;
