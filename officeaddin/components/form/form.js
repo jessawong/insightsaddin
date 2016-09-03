2c660fded06701c2db36b9a79625f744286f443d
Greg Degruy <gregory.degruy@gmail.com>
2016-09-02 21:29:25 -0700

    Preparing to add industries drop down

--- officeaddin/components/form/form.js
+++ officeaddin/components/form/form.js
@@ -2,7 +2,7 @@
 
   "use strict";
 
-  var app = angular.module('readHome', []);
+  var app = angular.module("readHome", []);
 
   var info =
     {
@@ -17,7 +17,6 @@
       "state": "",
       "date": "",
       "time": "",
-      "reason": "",
       "meeting": "Skype",
       "engagement": ""
     };
@@ -32,23 +31,23 @@
       "workloads": ""
     };
 
-  Office.initialize = function (reason) {
+  Office.initialize = function (args) {
     jQuery(document).ready(function () {
       info.te = Office.context.mailbox.userProfile.displayName;
     });
   };
 
-  app.controller('FormController', function ($scope) {
+  app.controller("FormController", function ($scope) {
     info["Technology"] = "Intelligent Cloud";
-    $scope.technology = [{ 'type': 'Intelligent Cloud', 'url': '../../images/cloud.png', 'alias': 'usdxrisvintelligentcloudteam@service.microsoft.com', 'fadeUrl': '../../images/cloudOverlay.png', 'intel': true }];
-    $scope.engagement = ['Briefing', 'Envisioning', 'ADS', 'Hackfest/PoC', 'Other'];;
-    $scope.cloudInfor = { 'status': '', 'provider': '', 'consumption': '', 'workloads': '' };
-    $scope.cloudStatus = ['New', 'Experimenting', 'Hybrid', 'Running'];
-    $scope.cloudProv = ['None', 'Azure', 'AWS', 'Google', 'Other'];
-    $scope.consumption = ['<25k', '25k-99k', '100k-499k', '500k+'];
-    $scope.workloads = { 'Identity & Access Mgt': false, 'Compute': false, 'Networking': false, 'Storage & DR': false, 'Big Data & SQL': false, 'IoT': false, 'Advanced Analytics': false, 'PaaS Services': false, 'OSS Platforms': false, 'Mobility': false, 'Media Solutions': false };
+    $scope.technology = [{ "type": "Intelligent Cloud", "url": "../../images/cloud.png", "alias": "usdxrisvintelligentcloudteam@service.microsoft.com", "fadeUrl": "../../images/cloudOverlay.png", "intel": true }];
+    $scope.engagement = ["Briefing", "Envisioning", "ADS", "Hackfest/PoC", "Other"];;
+    $scope.cloudInfor = { "status": "", "provider": "", "consumption": "", "workloads": "" };
+    $scope.cloudStatus = ["New", "Experimenting", "Hybrid", "Running"];
+    $scope.cloudProv = ["None", "Azure", "AWS", "Google", "Other"];
+    $scope.consumption = ["<25k", "25k-99k", "100k-499k", "500k+"];
+    $scope.workloads = { "Identity & Access Mgt": false, "Compute": false, "Networking": false, "Storage & DR": false, "Big Data & SQL": false, "IoT": false, "Advanced Analytics": false, "PaaS Services": false, "OSS Platforms": false, "Mobility": false, "Media Solutions": false };
     $scope.information = info;
-    $scope.timeOptions = ['30 min', '60 min', '90 min', '120 min', '2+ hours'];
+    $scope.timeOptions = ["30 min", "60 min", "90 min", "120 min", "2+ hours"];
     $scope.skype = false;
     $scope.intelCloud = true;
     $scope.showArrow = true;
@@ -56,7 +55,7 @@
     $scope.ads = false;
     $scope.showSubmit = false;
     $scope.showBack = false;
-    $scope.stages = ['0%', '10%', '20%', '40%', '60%', '80%', '95%', '100%'];
+    $scope.stages = ["0%", "10%", "20%", "40%", "60%", "80%", "95%", "100%"];
     $scope.pbes = ["Alexandra Detweiler", "Beverly Ann Smith", "Bill Lyle", "David Cazel", "Frances Calandra", "Harsha Vishwanathan", "Hong Choing", "Jon Box", "Kevin Boyle", "Micheal Liwanag", "Paul Debaun", "Tina Prause", "Tony Piltzecker", "Wes Yanage", "Will Tschumy", "Sam Chenaur"];
     $scope.industries = ["CDN", "Cross Industry", "Dev Tools", "Education", "Finance Services", "Health Care", "Insurance", "Manufacturing", "Mining", "Oil and Gas", "Public Sector", "Retail"];   
     $scope.showMain = true;
@@ -81,7 +80,7 @@
     };
 
     $scope.addRequest = function () {
-      var cloudDetails = '';
+      var cloudDetails = "";
       if ($scope.intelCloud) {
         cloudDetails = "<br/><h4>Industry/Vertical: </h4>" + $scope.cloudInfor.industry +
           "<br/><h4>Cloud Status: </h4>" + this.cloudInfor.status +
@@ -107,7 +106,6 @@
         "<br/><h4>PBE:</h4>" + $scope.information.pbe +
         "<br/><h4>Engagement Requested: </h4>" + $scope.information.engagement +
         "<br/><h4>Requested Date for Engagement:</h4>" + $scope.information.date +
-        "<br/><h4>Reason:</h4>" + $scope.information.reason +
         "<br/><h4>Duration of meeting:</h4>" + $scope.information.time +
         "<br/><h4>Location:</h4>" + $scope.information.location +
         "<br/><h4>Meeting:</h4>" + $scope.information.meeting + cloudDetails +
@@ -148,11 +146,11 @@
 
       var ehClient = new EventHubClient(
         {
-          'name': hubname,
-          'namespace': namespace,
-          'sasKey': "HsoQY4aVbX2cOEhg5hqwfzQDSLLIKyvvIrNt/u2jU+k=",
-          'sasKeyName': "send",
-          'timeOut': 100,
+          "name": hubname,
+          "namespace": namespace,
+          "sasKey": "HsoQY4aVbX2cOEhg5hqwfzQDSLLIKyvvIrNt/u2jU+k=",
+          "sasKeyName": "send",
+          "timeOut": 100,
         });
 
       var msg = new EventData(payload);
