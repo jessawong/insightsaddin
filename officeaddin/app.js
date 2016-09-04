75af94b5918b94077826dfb10c39f65de3e8d8ef
Greg Degruy <gregory.degruy@gmail.com>
2016-09-04 16:46:08 -0700

    Hard coding for testing now

--- officeaddin/app.js
+++ officeaddin/app.js
@@ -1,19 +1,19 @@
 var app = angular.module("insightsOfficeApp", []);
 
-app.config(["$stateProvider", "$urlRouterProvider", function ($stateProvider, $urlRouterProvider) {
+// app.config(["$stateProvider", "$urlRouterProvider", function ($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise("/404");
-    $urlRouterProvider.when("", "/");
+//     $urlRouterProvider.otherwise("/404");
+//     $urlRouterProvider.when("", "/");
 
-    $stateProvider
-        .state("main", {
-            url: "/",
-            templateUrl: "app.html",
-            controller: "FormController",
-            controllerAs: "formCtrl"
-        })
-        .state("404", {
-            url: "/404",
-            templateUrl: "app/shared/404.html"
-        });
-}]);
+//     $stateProvider
+//         .state("main", {
+//             url: "/",
+//             templateUrl: "app.html",
+//             controller: "FormController",
+//             controllerAs: "formCtrl"
+//         })
+//         .state("404", {
+//             url: "/404",
+//             templateUrl: "app/shared/404.html"
+//         });
+// }]);
