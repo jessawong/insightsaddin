aa9d8ea9d4b6e18761a6c4c866e37c7f523167b2
Greg Degruy <gregory.degruy@gmail.com>
2016-09-04 09:38:42 -0700

    Fixed form folder typo

--- officeaddin/app.js
+++ officeaddin/app.js
@@ -1,12 +1,4 @@
 angular.module("insightsOfficeApp").config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider){
 
     $urlRouterProvider.when("", "/");
-
-    $stateProvider
-        .state("main", {
-            url: "/",
-            templateUrl: "components/main.html",
-            controller: "mainController",
-            controllerAs: "ctrl"
-        });
 }]);
