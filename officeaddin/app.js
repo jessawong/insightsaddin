660a52c2d440cb548fffb5203a4fd354109ddc7f
Greg Degruy <gregory.degruy@gmail.com>
2016-09-04 16:49:19 -0700

    Trying to render dynamic ui

--- officeaddin/app.js
+++ officeaddin/app.js
@@ -1,19 +1,2 @@
+// On first call to angular.module, use [] to specify init
 var app = angular.module("insightsOfficeApp", []);
-
-// app.config(["$stateProvider", "$urlRouterProvider", function ($stateProvider, $urlRouterProvider) {
-
-//     $urlRouterProvider.otherwise("/404");
-//     $urlRouterProvider.when("", "/");
-
-//     $stateProvider
-//         .state("main", {
-//             url: "/",
-//             templateUrl: "app.html",
-//             controller: "FormController",
-//             controllerAs: "formCtrl"
-//         })
-//         .state("404", {
-//             url: "/404",
-//             templateUrl: "app/shared/404.html"
-//         });
-// }]);
